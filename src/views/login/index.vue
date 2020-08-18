718dd71688b7c2d88cb10a2532a463a95756e7cb
Duan Li Hao <136282199@qq.com>
2020-08-18 17:15:52 +0800

    登录页面完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>登录</div>
+  <div class="login">
+      <!-- 组件 -->
+      <el-card class="login-card">
+          <div class="title">
+              <img src="../../assets/img/logo_index.png" alt="">
+          </div>
+          <!-- 表单 -->
+          <el-form style="margin-top:20px">
+            <el-form-item>
+                <el-input placeholder="请输入手机号"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-input style="width:200px" placeholder="请输入验证码"></el-input>
+              <el-button style="float:right">发送验证码</el-button>
+            </el-form-item>
+          <!-- 选框 -->
+           <el-form-item>
+                <el-checkbox>我已阅读并同意你列举的霸王条款</el-checkbox>
+           </el-form-item>
+          <!--登录-->
+           <el-form-item>
+                <el-button type="primary" style="width:300px; margin-left:30px">登录</el-button>
+           </el-form-item>
+          </el-form>
+      </el-card>
+  </div>
 </template>
 
 <script>
@@ -7,6 +32,23 @@ export default {
 
 }
 </script>
-<style>
-
+<style lang='less' scoped>
+    .login{
+      background-image: url('../../assets/img/login_bg.jpg');
+      background-size: cover;
+      height:100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .login-card{
+        width:400px;
+        height:320px;
+        .title{
+               text-align: center;
+              img{
+                  width:200px;
+            }
+        }
+      }
+    }
 </style>
