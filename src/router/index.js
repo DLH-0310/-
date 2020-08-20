b5b77e92288ba26b8b3f0871b3d6adc3e7c91170
Duan Li Hao <136282199@qq.com>
2020-08-20 11:05:50 +0800

    提交时的注释

--- src/router/index.js
+++ src/router/index.js
@@ -21,18 +21,29 @@ const routes = [
     path: '/login',
     component: Login
   }
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
 ]
 
 const router = new VueRouter({
   routes
+})
+// 路由拦截器 该方法接收函数作为参数
+// to ：表示去哪里的路由信息
+// from ：表示来自哪里的路由信息
+// next ：是一个方法，用来放行
+router.beforeEach((to, from, next) => {
+// 做一个判断用户是否处在登录状态，有就通过，没有就跳回登录
+  // 如果是登录页,直接放行
+  if (to.path === '/login') {
+    next()
+  }
+  // 非登录页判断有没有token
+  const token = window.localStorage.getItem('user-token')
+  if (token) {
+    next()
+  } else {
+  // 没有就跳回登录页
+    next('/login')
+  }
 })
 
 export default router
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,78 @@
 <template>
-  <div class="home">主页
+  <div class="home">
+    <!-- 布局 -->
+    <el-container>
+      <!-- 左右布局 -->
+      <el-aside class="left" style="width:220px">
+        <div style="background:black"  >
+          <img src="../../assets/img/logo_admin.png" alt="">
+        </div>
+          <!-- 左侧菜单 -->
+          <el-menu
+            style="width:222px"
+            class="el-menu-vertical-demo"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+            >
+            <!-- 菜单 -->
+            <!-- 一级菜单 -->
+           <el-menu-item >
+            <span slot="title" class="home">首页</span>
+          </el-menu-item>
+          <!-- 二级菜单 -->
+        <el-submenu index="1">
+            <template slot="title">
+              <span>内容管理</span>
+            </template>
+          <el-menu-item-group>
+            <el-menu-item>发布文章</el-menu-item>
+            <el-menu-item>内容列表</el-menu-item>
+            <el-menu-item>评论列表</el-menu-item>
+            <el-menu-item>素材管理</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+          <!-- 二级菜单 -->
+        <el-submenu index="2">
+            <template slot="title">
+              <span>粉丝管理</span>
+            </template>
+          <el-menu-item-group>
+            <el-menu-item>图文数据</el-menu-item>
+            <el-menu-item>粉丝概况</el-menu-item>
+            <el-menu-item>粉丝画像</el-menu-item>
+            <el-menu-item>粉丝列表</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+          <!-- 一级菜单 -->
+          <el-menu-item >
+            <span slot="title">账户信息</span>
+          </el-menu-item>
+          </el-menu>
+      </el-aside>
+      <!-- 上下 -->
+      <el-container>
+        <el-header>
+          <home-header></home-header>
+        </el-header>
+        <el-main></el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
+import Header from '../../components/Header'
 export default {
-  name: 'Home'
+  components: {
+    'home-header': Header
+  }
 }
 </script>
-<style>
-
+<style lang='less' scoped>
+    .left{
+      background-color:  #545c64;
+      height: 100vh;
+      overflow: hidden;
+    }
 </style>
